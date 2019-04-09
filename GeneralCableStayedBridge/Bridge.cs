b6cdca7b27f9b27709739df2a01b76d41991846f
billhu0228 <billhu0228@icloud.com>
2019-04-09 22:59:38 +0800

    a

--- GeneralCableStayedBridge/Bridge.cs
+++ GeneralCableStayedBridge/Bridge.cs
@@ -32,14 +32,57 @@ namespace GeneralCableStayedBridge
         internal void GenerateCrossTruss(ref Model myModel)
         {
             Beam curBeam;
-
+            TSG.Point A, B, C, D, E, F, G, H, I;
             for (int i = 0; i < NumSide; i++)
             {
-                double x0 = -HalfLength + i * SizeSide;
+                double x0 = i * SizeSide;
+                A = new TSG.Point(x0, -0.5 * BeamWidth, 0);
+                F = new TSG.Point(x0, 0.5 * BeamWidth, 0);
+                B = new TSG.Point(x0, -0.5 * BeamWidth, -BeamHeight);
+                E = new TSG.Point(x0, 0.5 * BeamWidth, -BeamHeight);
+                C = new TSG.Point(x0, -0.25 * BeamWidth, -BeamHeight);
+                D = new TSG.Point(x0,0.25 * BeamWidth, -BeamHeight);
+                I = new TSG.Point(x0, -0.25 * BeamWidth, 0);
+                H = new TSG.Point(x0,0, 0);
+                G = new TSG.Point(x0, 0.25 * BeamWidth, 0);
 
-                curBeam = new Beam(new TSG.Point(x0, 0, 0), new TSG.Point(0, -0.5*BeamWidth,0));                
-                myBeam.Profile.ProfileString = "WI400-15-20*300";            
-                //myBeam.Insert();
+
+
+                curBeam = new Beam(A,F );
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(B,E );
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+
+                curBeam = new Beam(A, B);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(F, E);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+
+                curBeam = new Beam(C,A);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(C,I);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(C, H);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+
+                curBeam = new Beam(D, F);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(D, G);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+                curBeam = new Beam(D, H);
+                curBeam.Profile.ProfileString = "WI400-15-20*300";
+                curBeam.Insert();
+
+
                 if (i==0)
                 {
                     break;
